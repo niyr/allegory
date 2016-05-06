179261e60ae56f65ba77b68767019f0ce7d03696
niyr <niyr@live.com>
2016-05-06 21:39:39 +1000

    Initial commit + .gitignore (Hugo owes me 4 hours, 40 minutes).

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	private static GameManager instance;
+	public static GameManager Instance { get { return instance; } }
+
+	#region MonoBehaviour Lifecycle
+	protected void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+		else
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+	#endregion
+}
---
+++ Assets/Scripts/Shuffler.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class Shuffler : MonoBehaviour
+{
+	// CACHED COMPONENTS
+	private Transform _transform;
+
+	[SerializeField]
+	private bool shuffleChildren = true;
+	private List<Transform> children = new List<Transform>();
+
+	[SerializeField]
+	private Vector3 maxPositionDelta = Vector3.zero;
+	[SerializeField]
+	private Vector3 maxRotationDelta = Vector3.zero;
+	[SerializeField]
+	private Vector3 maxScaleDelta = Vector3.zero;
+
+	protected void Awake()
+	{
+		_transform = GetComponent<Transform>();
+
+		if(shuffleChildren)
+		{
+			foreach(Transform t in _transform)
+			{
+				Shuffle(t);
+				children.Add(t);
+			}
+		}
+		else
+		{
+			Shuffle(_transform);
+		}
+	}
+
+	private void Shuffle(Transform target)
+	{
+		/*
+		Vector3w pos = new Vector3(Random.Range(-maxPositionDelta.x, maxPositionDelta.x),
+			Random.Range(-maxPositionDelta.y, maxPositionDelta.y),
+			Random.Range(-maxPositionDelta.z, maxPositionDelta.z));
+		*/
+
+		Vector3 pos = Vector3.Scale(Random.insideUnitSphere, maxPositionDelta);
+		Quaternion rot = Quaternion.Euler(Vector3.Scale(Random.insideUnitSphere, maxRotationDelta));
+		Vector3 scale = Vector3.Scale(Random.insideUnitSphere, maxScaleDelta);
+
+		target.position += pos;
+		target.rotation *= rot;
+		target.localScale += scale;
+	}
+}
